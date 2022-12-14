45dc42f07242a52a129e57af4b337d1a11c452d3
ShevoFirst <play.job@yandex.ru>
2022-12-14 06:50:54 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {//Задание 1-2.
+        Person maxim = new Person(1988,"Максим","Минск","бренд-менеджер");
+        Person anna = new Person(1993,"Аня","Москва","методист образовательных программ");
+        Person katya = new Person(1992,"Катя","Калининград","продакт-менеджер");
+        Person artem = new Person(1995,"Артем","Москва","директор по развитию бизнеса");
+        Person g = new Person(1995,"Артем",null,"директор по развитию бизнеса");
+        System.out.println();
+        //Задание 3.
+        Car granta = new Car("Lada","Granta","желтый","Россия",1.7,2015);
+        Car audiA8 = new Car("Audi","A8 50 L TDI quattro","черный","Германия",3.0,2020);
+        Car bmwZ8 = new Car("BMW","Z8","черный","Германия",3.0,2021);
+        Car sportage = new Car("KIA","Sportage","","Южная Корея",2.4,2018);
+        Car avente = new Car("Hyundai","Avante","","Южная Корея",1.6,2016);
+    }
+}
---
+++ src/Person.java
@@ -0,0 +1,32 @@
+public class Person {//Задание 2.
+    int yearOfBirth;
+    String name;
+    String town;
+    String jobTitle;
+
+    public Person(int yearOfBirth, String name, String town,String jobTitle) {
+        this.yearOfBirth = yearOfBirth;
+        if (yearOfBirth < 0) {
+            this.yearOfBirth = 0;
+        }
+        this.name = name;
+        if (name == null || name.isEmpty()) {
+            this.name = "Информация не указана";
+        }
+        this.town = town;
+        if (town == null || town.isEmpty()) {
+            this.town = "Информация не указана";
+        }
+        this.jobTitle = jobTitle;
+        if (jobTitle == null || jobTitle.isEmpty()) {
+            this.jobTitle = "Информация не указана";
+        }
+        System.out.println(toString());
+    }
+
+    @Override
+    public String toString() {
+        return "Привет! Меня зовут "+name+" Я из города "+town+
+               " Я родился в "+yearOfBirth+" году.Я работаю на должности"+jobTitle+". Будем знакомы!";
+    }
+}
